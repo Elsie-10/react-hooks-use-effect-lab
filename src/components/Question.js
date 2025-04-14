abb013d773f897aa2d274074fc5abe7efb2a4f94
Elsie <elsieoscar06@gmail.com>
2025-04-14 12:42:21 +0300

    using useeffect and doing a cleanup

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    // set up the timeout to decrease time remaining by 1 every second
+    const timerId = setTimeout(() => {
+      setTimeRemaining((prevTime) => {
+        if(prevTime > 1){
+          return prevTime - 1
+        }else {
+          onAnswered(false);
+          return 10; // reset timer for the next question
 
+        }
+      });
+    },1000);
+    // clean up the timer
+    return() => clearTimeout(timerId)
+  },[timeRemaining,onAnswered])
+      
+//cleanup function to clear the timeout
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
